d052c48cdaa1a8b3f4a3918e238f4a7ee017553a
BryceQin <Bryce.Qin@outlook.com>
2026-05-19 16:47:16 +0800

    feat: add function completion module

---
+++ src/completion/functionCompletion.ts
@@ -0,0 +1,22 @@
+import * as vscode from 'vscode'
+import type { FunctionSignature } from './functionSignatures'
+import { signatureToString, getCategoryLabel } from './functionSignatures'
+
+export function getFunctionItems(functions: FunctionSignature[]): vscode.CompletionItem[] {
+    return functions.map((fn) => {
+        const item = new vscode.CompletionItem(fn.name, vscode.CompletionItemKind.Function)
+        item.insertText = new vscode.SnippetString(
+            `${fn.name}(${fn.params.map((_, i) => `$\{${i + 1}:${fn.params[i]}}`).join(', ')})`
+        )
+        item.detail = `${getCategoryLabel(fn.category)} | ${signatureToString(fn)}`
+        const md = new vscode.MarkdownString()
+        md.appendMarkdown(`### ${fn.name}\n\n${fn.description}\n\n`)
+        md.appendCodeblock(signatureToString(fn), 'sql')
+        if (fn.returnType) {
+            md.appendMarkdown(`\n\n返回类型: \`${fn.returnType}\``)
+        }
+        item.documentation = md
+        item.sortText = `2_${fn.name}`
+        return item
+    })
+}
